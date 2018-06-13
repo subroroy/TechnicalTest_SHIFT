131e8d4013da7b3baba20efcd441b95267f5765e
subroroy <subro.roy@gmail.com>
2018-06-14 02:21:55 +0530

    Added javadoc

--- TechnicalTest_SHIFT/src/src/com/ElementSynchronization.java
+++ TechnicalTest_SHIFT/src/src/com/ElementSynchronization.java
@@ -10,12 +10,18 @@ public class ElementSynchronization {
 	
 	private WebDriverWait webDriverWait;
 	
+	/**
+	 * Exception handling : Wait till WebElement ele is visible on the page
+	 */
 	public WebElement waitTillElementVisible(WebDriver driver, WebElement ele) {
 		webDriverWait = new WebDriverWait(driver, 20);
 		ele = webDriverWait.until(ExpectedConditions.visibilityOf(ele));
 		return ele;
 	}
 	
+	/**
+	 * Exception handling : Hover over the mentioned WebElement on the page 
+	 */
 	public WebElement hoverToElement(WebDriver driver, WebElement ele) {
 		Actions obj = new Actions(driver);
 		obj.moveToElement(ele).build().perform();
